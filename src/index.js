0a957820865c884b50c4e18606213aeec81d489a
esoshyki <esoshyki@yandex.by>
2020-09-07 19:10:22 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
+const isEmptyArray = array => (!array || array.length === 0) ? true : false
 
 exports.min = function min (array) {
-  return 0;
+  return isEmptyArray(array) ? 0 : array.sort((a, b) => a - b)[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  return isEmptyArray(array) ? 0 : array.sort((a,b) => b - a)[0]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return isEmptyArray(array) ? 0 : array.reduce((acc, cur) => acc + cur) / array.length  
 }
